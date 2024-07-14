fbae62bcfa6f4e4816d75da68f779c85a85eabbc
youminki <dbalsrl7648@naver.com>
2024-07-15 06:57:48 +0900

    setting : GlobalStyle 수정 #2

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,34 +1,35 @@
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
+import { Theme } from "./Theme";
 
 const GlobalStyle = createGlobalStyle`
     ${reset}
 
     html, body, div, span, h1, h2, h3, h4, h5, h6, p,
-    a, dl, dt, dd, ol, ul, li, form, label, table{
+    a, dl, dt, dd, ol, ul, li, form, label, table {
         margin: 0;
         padding: 0;
         border: 0;
         text-decoration: none;
         box-sizing: border-box;
-        ${(props) => props.theme.fonts.default};
-        &:visited{
-        text-decoration: none;
-        color: black;
+        ${Theme.fonts.default};
+        &:visited {
+            text-decoration: none;
+            color: black;
         }
     }
-    body{
+    body {
         line-height: 1;
         margin: 0;
         padding: 0;
     }
-    ol, ul{
+    ol, ul {
         list-style: none;
     }
     button {
         border: 0;
         cursor: pointer;
     }
-    `;
+`;
 
 export default GlobalStyle;
--- src/styles/Theme.js
+++ src/styles/Theme.js
@@ -1,32 +1,38 @@
 import { css } from "styled-components";
-import './Font.css';
 
 export const Theme = {
     fonts: {
         mainTitle: css`
-        font-family: "Noto Sans KR", sans-serif;
-        font-weight: 400;
-        font-size: 54px;
+            font-family: "Noto Sans KR", sans-serif;
+            font-weight: 400;
+            font-size: 54px;
         `,
         heading: css`
-        font-family: "Noto Sans KR", sans-serif;
-        font-weight: 400;
-        font-size: 28px;
+            font-family: "Noto Sans KR", sans-serif;
+            font-weight: 400;
+            font-size: 28px;
         `,
         default: css`
-        font-family: "Noto Sans KR", sans-serif;
-        font-weight: 400;
-        font-size: 20px;
+            font-family: "Noto Sans KR", sans-serif;
+            font-weight: 400;
+            font-size: 20px;
         `,
         helperText: css`
-        font-family: "Noto Sans KR", sans-serif;
-        font-weight: 400;
-        font-size: 16px;
-        `,
-        button: css`
             font-family: "Noto Sans KR", sans-serif;
-            font-weight: 500;
-            font-size: 36px;
+            font-weight: 400;
+            font-size: 16px;
+        `,
+        BigButton: css`
+            font-family: "Noto Sans KR", sans-serif;
+            font-weight: 600;
+            font-size: 30px;
+        `,
+        SmallButton: css`
+            font-family: "Noto Sans KR", sans-serif;
+            font-weight: 600;
+            font-size: 28px;
+            line-height: 34px;
+            text-align: center;
         `,
         subTitle: css`
             font-family: "Noto Sans KR", sans-serif;
@@ -43,13 +49,55 @@ export const Theme = {
             font-weight: 400;
             font-size: 21px;
         `,
+        inputLabel: css`
+            font-family: "Noto Sans KR", sans-serif;
+            font-weight: 700;
+            font-size: 24px;
+            line-height: 140%;
+            letter-spacing: 0.02em;
+            text-transform: capitalize;
+        `,
+        inputField: css`
+            font-family: "Noto Sans KR", sans-serif;
+            font-weight: 800;
+            font-size: 16px;
+            line-height: 160%;
+        `,
+        title: css`
+        font-family: "Noto Sans KR", sans-serif;
+        font-weight: 900;
+        font-size: 96px;
+        line-height: 116px;
+        `,
+        loginText: css`
+        font-family: "Noto Sans KR", sans-serif;
+        font-weight: 900;
+        font-size: 50px;
+        line-height: 61px;
+        text-align: center;
+        `,
+        signupText: css`
+        font-family: "Noto Sans KR", sans-serif;
+        font-weight: 600;
+        font-size: 28px;
+        line-height: 34px;
+        text-align: center;
+        `,
+        noAccountText: css`
+        font-family: "Noto Sans KR", sans-serif;
+        font-weight: 400;
+        font-size: 20px;
+        line-height: 24px;
+        text-align: center;
+        `,
     },
     colors: {
-        red1: "B37371",
-        red1: "CD9191",
-        red1: "EECBCA",
-        pink1: "FDEFEE",
-        pink2: "F7A29D",
+        red1: "#B37371",
+        red2: "#CD9191",
+        red3: "#EECBCA",
+        pink1: "#FDEFEE",
+        pink2: "#F7A29D",
+        pink3: "#F77770",
 
         black: "#000000",
         white: "#FFFFFF",
