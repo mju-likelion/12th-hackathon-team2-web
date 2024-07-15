fcd3be8c7d3e471193a41595bcae3611e45911a6
youminki <dbalsrl7648@naver.com>
2024-07-15 22:15:37 +0900

    fix : background 배경 svg 추가 및 설정

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import background from "../img/background.svg";
 
 const Container = styled.div`
   display: flex;
@@ -7,7 +8,8 @@ const Container = styled.div`
   justify-content: center;
   width: 100vw;
   height: 100vh;
-  background: ${(props) => props.theme.colors.white};
+  background: url(${background}) no-repeat center center;
+  background-size: cover;
   position: relative;
 `;
 
