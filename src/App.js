8f1ea4362a31786472c5ea1280ce953a8dd02fdb
youminki <dbalsrl7648@naver.com>
2024-07-16 19:29:22 +0900

    fix : 페이지명 수정

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
-import Calendar from "./pages/Calendar";
 import Diary from "./pages/Diary";
 import Join from "./pages/Join";
 import Login from "./pages/Login";
 import Main from "./pages/Main";
+import Planner from "./pages/Planner";
 import Pomodoro from "./pages/Pomodoro";
 import Session from "./pages/Session";
 import GlobalStyle from "./styles/GlobalStyle";
@@ -20,7 +20,7 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/join" element={<Join />} />
           <Route path="/main" element={<Main />} />
-          <Route path="/calendar" element={<Calendar />} />
+          <Route path="/planner" element={<Planner />} />
           <Route path="/diary" element={<Diary />} />
           <Route path="/session" element={<Session />} />
           <Route path="/pomodoro" element={<Pomodoro />} />
