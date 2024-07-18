cd674f11c24f63671c49cded7afa63da429b4ce3
김희수 <rulru0413@naver.com>
2024-07-19 01:20:53 +0900

    feat: 설문버튼UI구현

---
+++ src/components/SurveyButton.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const SurveyButton = ({ content }) => {
+  const [clicked, setClicked] = useState(false);
+
+  const handleClick = () => {
+    setClicked(!clicked);
+  };
+
+  return (
+    <div>
+      <Button onClick={handleClick} clicked={clicked}>
+        {content}
+      </Button>
+    </div>
+  );
+};
+
+const Button = styled.button`
+  padding: 14px 39px;
+  border-radius: 11px;
+  cursor: pointer;
+  color: ${(props) =>
+    props.clicked ? props.theme.colors.white : props.theme.colors.black};
+  background-color: ${(props) =>
+    props.clicked ? props.theme.colors.pink2 : props.theme.colors.white};
+  border: ${(props) =>
+    props.clicked ? "none" : `1px solid  ${props.theme.colors.gray}`};
+`;
+
+export default SurveyButton;
