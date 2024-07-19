1621a981369241ee2d900b107c0c1bf8a6446016
김희수 <rulru0413@naver.com>
2024-07-19 14:15:40 +0900

    feat: 설문 버튼 컴포넌트 수정

--- src/components/SurveyButton.js
+++ src/components/SurveyButton.js
@@ -21,12 +21,16 @@ const Button = styled.button`
   padding: 14px 39px;
   border-radius: 11px;
   cursor: pointer;
+  ${(props) => props.theme.fonts.helperText};
+  font-weight: 500;
   color: ${(props) =>
     props.clicked ? props.theme.colors.white : props.theme.colors.black};
   background-color: ${(props) =>
     props.clicked ? props.theme.colors.pink2 : props.theme.colors.white};
   border: ${(props) =>
-    props.clicked ? "none" : `1px solid  ${props.theme.colors.gray}`};
+    props.clicked
+      ? `1px solid  ${props.theme.colors.pink2}`
+      : `1px solid  ${props.theme.colors.gray}`};
 `;
 
 export default SurveyButton;
