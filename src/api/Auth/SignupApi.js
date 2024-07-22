2efacfc3fb7908ca69c798d14a69ac7594f4a081
youminki <dbalsrl7648@naver.com>
2024-07-22 18:51:16 +0900

    fix : pw, name field

--- src/api/Auth/SignupApi.js
+++ src/api/Auth/SignupApi.js
@@ -3,8 +3,8 @@ export const SignupApi = (data, callbackFunctions) => {
     const { navigateSuccess, navigateError } = callbackFunctions;
     Axios.post(`https://api.mutsideout.com/auth/signup`, {
         email: data.email,
-        password: data.password,
-        name: data.name,
+        password: data.pw,
+        name: data.nickname,
     })
         .then(() => {
             navigateSuccess();
