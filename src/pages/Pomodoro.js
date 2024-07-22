8f64745b648319372cb0fd15c9e7f1084023774e
youminki <dbalsrl7648@naver.com>
2024-07-22 23:04:07 +0900

    fix : Pomodoro css #1

--- src/pages/Pomodoro.js
+++ src/pages/Pomodoro.js
@@ -1,13 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { buildStyles, CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
-import { FaClock } from 'react-icons/fa';
 import styled from 'styled-components';
 import Sound from '../assets/alarm.mp3';
 import Header from '../components/Header';
-import Timebox from '../components/Timebox';
 import TinyButton from '../components/TinyButton';
 import pomodoroLogo from '../img/pomodoroLogo.svg';
+import { Theme } from "../styles/Theme";
 
 const Pomodoro = () => {
   const [workMinutes, setWorkMinutes] = useState('');
@@ -125,55 +124,79 @@ const Pomodoro = () => {
     <Div>
       <Header />
       <Container>
-        <History>
-          <HistoryTitle>
-            <FaClock style={{ width: '30%' }} /> <br />
-            최근 이용
-          </HistoryTitle>
-          {history.map((item, index) => (
-            <HistoryItem
-              key={index}
-              onClick={() =>
-                handleHistoryClick(item.workMinutes, item.breakMinutes)
-              }
-            >
-              {item.workMinutes}분 / {item.breakMinutes}분
-            </HistoryItem>
-          ))}
-        </History>
+        <Left>
+          <SideTitle>주기설정</SideTitle>
+          <SettingBox>
+            <h3>집중시간</h3>
+            <Input
+              type='number'
+              value={workMinutes}
+              onChange={(e) => setWorkMinutes(e.target.value)}
+              placeholder='분을 입력해주세요'
+              min='1'
+              max='60'
+            />
+          </SettingBox>
+          <SettingBox>
+            <h3>휴식시간</h3>
+            <Input
+              type='number'
+              value={breakMinutes}
+              onChange={(e) => setBreakMinutes(e.target.value)}
+              placeholder='분을 입력해주세요'
+              min='0'
+              max='60'
+            />
+          </SettingBox>
+          <SideTitle>최근이용</SideTitle>
+          <Report>
+            {history.map((item, index) => (
+              <HistoryItem
+                key={index}
+                onClick={() =>
+                  handleHistoryClick(item.workMinutes, item.breakMinutes)
+                }
+              >
+                {item.workMinutes}분 / {item.breakMinutes}분
+              </HistoryItem>
+            ))}
+          </Report>
+        </Left>
         <Right>
           <Title><Logo src={pomodoroLogo} alt="Pomodoro Logo" /></Title>
           <Info>
             {isRunning && `${cycleCount}번째 타임`}{' '}
           </Info>
-          <TimerDisplay>
-            <TimerSection>
-              <Timebox>{'집중시간'}</Timebox>
+          <StyledTitle>{'집중시간'}</StyledTitle>
+          <TimerSection>
+            <TimerDisplay>
               <CircularProgressbar
                 value={getPercentage(timeLeft)}
                 text={timeLeft !== null && !isBreak ? formatTime(timeLeft) : '00:00'}
                 styles={buildStyles({
-                  pathColor: `#E93C3C`,
+                  pathColor: `#FFFFFF`,
                   textColor: '#E93C3C',
                   trailColor: '#E93C3C',
                   backgroundColor: '#FFFFFF',
                 })}
               />
-            </TimerSection>
-            <TimerSection>
-              <Timebox>{'휴식시간'}</Timebox>
+            </TimerDisplay>
+          </TimerSection>
+          <StyledTitle>{'휴식시간'}</StyledTitle>
+          <TimerSection>
+            <TimerDisplay>
               <CircularProgressbar
                 value={getPercentage(timeLeft)}
                 text={timeLeft !== null && isBreak ? formatTime(timeLeft) : '00:00'}
                 styles={buildStyles({
-                  pathColor: `#E93C3C`,
+                  pathColor: `#FFFFFF`,
                   textColor: '#E93C3C',
                   trailColor: '#E93C3C',
                   backgroundColor: '#FFFFFF',
                 })}
               />
-            </TimerSection>
-          </TimerDisplay>
+            </TimerDisplay>
+          </TimerSection>
           <audio id='notificationSound' src={Sound}></audio>
           <ButtonGroup>
             <TinyButton onClick={handleStart} disabled={isRunning}>
@@ -200,47 +223,80 @@ const Container = styled.div`
   display: flex;
 `;
 
-const History = styled.div`
-  width: 16vw;
+const Left = styled.div`
+  width: 25vw;
   min-width: 170px;
+  height: 80%;
   background-color: ${(props) => props.theme.colors.pink1};
-  padding: 10px;
+  padding: 30px;
   border-radius: 30px;
-  height: 550px;
-  overflow-y: auto;
   margin-left: 7vw;
 `;
 
-const HistoryTitle = styled.h3`
-  margin-top: 40px;
-  text-align: center;
+const SideTitle = styled.h3`
+  margin-top: 20px;
   ${(props) => props.theme.fonts.PageNumber};
-  margin-bottom: 80px;
+  border-bottom: solid 2px ${(props) => props.theme.colors.pink3};
+  padding-bottom: 7px;
+  margin-bottom: 30px;
 `;
-
+const Report = styled.div`
+  background-color: ${(props) => props.theme.colors.white};
+  min-height: 17vw;
+  max-height: 20vw;
+  height: 50%;
+  width: 95%;
+  border-radius: 5px;
+  overflow-y: scroll;
+  padding: 20px;
+`;
 const HistoryItem = styled.div`
   ${(props) => props.theme.fonts.PageNumber};
   margin-bottom: 15px;
   cursor: pointer;
-  text-align: center;
+
+  font-weight: 400;
+  font-size: 17px;
   &:hover {
     color: ${(props) => props.theme.colors.pink3};
   }
 `;
+const SettingBox = styled.div`
+  background-color: ${(props) => props.theme.colors.pink1};
+  margin-top: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  border-radius: 30px;
+  h3 {
+    font-weight: 400;
+    font-size: 17px;
+  }
+`;
+
+const Input = styled.input`
+  margin-top: 10px;
+  width: 90%;
+  height: 20px;
+  padding: 10px;
+  outline: none;
+  border: none;
+  border-radius: 5px;
+  margin-bottom: 10px;
+`;
 
 const Right = styled.div`
   flex: 1;
-  background-color: ${(props) => props.theme.colors.pink1};
   display: flex;
   flex-direction: column;
   align-items: center;
-  max-width: 910px;
-  height: 773px;
+  max-width: 700px;
   margin: auto;
 `;
 
 const Title = styled.div`
-  margin-top: 73px;
+  font-size: 1.5em;
+  margin-bottom: 0.5em;
   ${(props) => props.theme.fonts.Context};
 `;
 
@@ -255,8 +311,8 @@ const Info = styled.div`
 
 const TimerDisplay = styled.div`
   display: flex;
-  justify-content: space-between;
-  width: 100%;
+  flex-direction: row;
+  align-items: center;
   font-size: 100px;
   padding: 10px;
 `;
@@ -265,22 +321,38 @@ const TimerSection = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  width: 50%;
+  width: 100%;
+  margin-bottom: 20px;
 `;
 
 const ButtonGroup = styled.div`
   display: flex;
   flex-direction: row;
-  gap: 4.7vw;
+  gap: 2.7vw;
   margin-bottom: 80px;
 `;
 
-// const Timebox = styled.div`
-//   color: ${(props) => props.theme.colors.pink3};
-// `;
 const Logo = styled.img`
   width: 309px;
   height: 61px;
+`;
+
+const StyledTitle = styled.div`
+    ${Theme.fonts.tinyButton};
+    color: ${Theme.colors.white};
+    padding: 10px;
+    background-color: ${Theme.colors.pink2};
+    border: none;
+    border-radius: 10px;
+    cursor: pointer;
+    margin-top: 10px;
+    width: 120px;
+    height: 40px;
+    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+
+    &:hover {
+        background-color: ${Theme.colors.pink3};
+    }
 `;
 
 export default Pomodoro;
